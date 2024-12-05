9f0c5f30b0d96722704c791bf77343632d4f63b5
KariKC <karikardec@gmail.com>
2024-12-05 14:42:19 -0300

    Atualiza o sync para poder sincronizar de forma global os comandos

--- cogs/Commands/sync.py
+++ cogs/Commands/sync.py
@@ -12,14 +12,14 @@ class Sync(commands.Cog):
        
     @commands.command(name="sync")
     async def status_ctx(self, ctx):
-        cmds = await ctx.bot.tree.sync(guild=discord.Object(id=os.getenv("SYSTEM_SERVER")))
+        cmds = await ctx.bot.tree.sync()
         await ctx.send(f"Synced {len(cmds)} commands")
 
     @app_commands.command(
             name = "sync",
             description="Sync the commands")
     async def sync(self, interaction: Interaction):
-        cmds = await self.bot.tree.sync(guild=discord.Object(id=os.getenv("SYSTEM_SERVER")))
+        cmds = await self.bot.tree.sync()
         await interaction.response.send_message(f"Synced {len(cmds)} commands")
 
 async def setup(bot):
